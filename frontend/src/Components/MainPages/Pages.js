2c3e980cc5a961f9917a39a9cf1d7df6503f21e5
DilkiThakshmila <dilkithakshmila@gmail.com>
2022-08-14 12:16:51 +0530

    *set route paths

--- frontend/src/Components/MainPages/Pages.js
+++ frontend/src/Components/MainPages/Pages.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Switch, Route} from 'react-router-dom'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
 import NotFound from './NotFound'
+import Categories from './Categories'
+import {GlobalState} from '../../GlobalState'
 
 export default function Pages() {
+
+  const state = useContext(GlobalState)
+  const [isLogged] = state.userAPI.isLogged
+  const [isAdmin] = state.userAPI.isAdmin
+
+
   return (
     
     <Switch>
-      <Route path="/login" exact component={SignIn} />
-      <Route path="/register" exact component={SignUp} />
-      <Route path="" exact component={NotFound} />
+      <Route path="/login" exact component={isLogged ? NotFound : SignIn} />
+      <Route path="/register" exact component={isLogged ? NotFound : SignUp} />
+      <Route path="/categoty" exact component={isAdmin ? Categories : NotFound} />
+      <Route path="*" exact component={NotFound} />
     </Switch>
     
   )
