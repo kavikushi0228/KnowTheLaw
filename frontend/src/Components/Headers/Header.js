8c90a04b1638cae62b1f25952e1f2ce50afc5613
Hashika <hashi.subodani@gmail.com>
2022-08-19 00:45:47 +0530

    added header.css

--- frontend/src/Components/Headers/Header.js
+++ frontend/src/Components/Headers/Header.js
@@ -1,17 +1,24 @@
 import React, {useState, useContext} from 'react'
 import { GlobalState } from '../../GlobalState'
 import {Link} from 'react-router-dom'
+import './Header.css';
 
 
 
 export default function Header() {
+  const HeaderStyle = {
+    color: 'white',
+    textDecoration: 'none',
+    fontSize:'20px'
+}
+
     const value = useContext(GlobalState)
   return (
-    <div>
-    <li  ><Link to="/">Home</Link></li>
-    <li  ><Link to="/login">SignIn</Link></li>
-    <li  ><Link to="/register">SignUp</Link></li> 
-    <li  ><Link to="/category">Categories</Link></li> 
+    <div className="header">
+    <li className="header.font"><Link to="/" style={HeaderStyle}>Home</Link></li>
+    <li className="header.font"><Link to="/login" style={HeaderStyle}>SignIn</Link></li>
+    <li className="header.font"><Link to="/register" style={HeaderStyle}>SignUp</Link></li> 
+    <li className="header.font"><Link to="/category" style={HeaderStyle}>Categories</Link></li> 
     
            
     </div>
