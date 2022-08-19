cdb430eff311ae4b8dbf774c6c60226e90273d59
Kavini Madhusanka Kushani <62046302+kavikushi0228@users.noreply.github.com>
2022-08-19 23:46:17 +0530

    Navigation bar is updated in UI

--- frontend/src/Components/Headers/Header.js
+++ frontend/src/Components/Headers/Header.js
@@ -1,26 +1,35 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext}  from 'react'
 import { GlobalState } from '../../GlobalState'
 import {Link} from 'react-router-dom'
 import './Header.css';
-
-
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 export default function Header() {
   const HeaderStyle = {
-    color: 'white',
     textDecoration: 'none',
-    fontSize:'20px'
+    fontSize:'20px',
+    color: 'rgb(8, 152, 255)'
 }
 
     const value = useContext(GlobalState)
+    const [menu, setMenu] = useState(false)
   return (
+   
     <div className="header">
-    <li className="header.font"><Link to="/" style={HeaderStyle}>Home</Link></li>
-    <li className="header.font"><Link to="/login" style={HeaderStyle}>SignIn</Link></li>
-    <li className="header.font"><Link to="/register" style={HeaderStyle}>SignUp</Link></li> 
-    <li className="header.font"><Link to="/category" style={HeaderStyle}>Categories</Link></li> 
-    
-           
+        <ul className='no-bullets'>
+        <li className="header-l" ><Link to="/" style={HeaderStyle}>Home</Link></li>
+        <li className="header-l"><Link to="/category" style={HeaderStyle}>Categories</Link></li>
+        <li className="header-l"><Link to="/register" style={HeaderStyle}>Sign Up</Link></li> 
+        <li className="header-l"><Link to="/login" style={HeaderStyle}>Login</Link></li>
+        </ul>   
     </div>
   )
 }
+
